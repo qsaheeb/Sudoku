44df100c133300cd789f08ebf08d772add91f950
Qazi Saheeb <qsaheeb93@gmail.com>
2021-08-29 00:11:14 +0530

    Published version 1 of sudoku app to GitHub pages

---
+++ build/precache-manifest.87c06bf2b5fd4b7e2df8dbb8d0a0bb45.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0197e28dd32ea17e93f911b671a72f",
+    "url": "/Sudoku/index.html"
+  },
+  {
+    "revision": "57038cda4f3f20eee848",
+    "url": "/Sudoku/static/css/2.ccc5b7a9.chunk.css"
+  },
+  {
+    "revision": "31003135207e096a8b7c",
+    "url": "/Sudoku/static/css/main.243aa9a5.chunk.css"
+  },
+  {
+    "revision": "57038cda4f3f20eee848",
+    "url": "/Sudoku/static/js/2.0f364b10.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/Sudoku/static/js/2.0f364b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31003135207e096a8b7c",
+    "url": "/Sudoku/static/js/main.bd39912a.chunk.js"
+  },
+  {
+    "revision": "92f8da498548882e3292",
+    "url": "/Sudoku/static/js/runtime-main.99f914a6.js"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/Sudoku/static/media/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/Sudoku/static/media/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/Sudoku/static/media/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/Sudoku/static/media/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/Sudoku/static/media/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/Sudoku/static/media/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/Sudoku/static/media/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/Sudoku/static/media/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/Sudoku/static/media/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/Sudoku/static/media/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/Sudoku/static/media/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/Sudoku/static/media/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/Sudoku/static/media/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/Sudoku/static/media/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/Sudoku/static/media/fa-solid-900.d824df7e.woff2"
+  }
+]);
